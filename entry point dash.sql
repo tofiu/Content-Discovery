ee5b628252750f874616ebdcb5fd5be27100ac9c
Tiffany Liu <46113070+tofiu@users.noreply.github.com>
2024-08-05 11:18:03 -0700

    Update entry point dash.sql

--- entry point dash.sql
+++ entry point dash.sql
@@ -231,8 +231,87 @@ where day_dt between '2022-01-01' and current_date()
     AND LOWER(subscription_package_desc) IN ('cf', 'lc', 'lcp', 'cf-sho', 'lc-sho', 'lcp-sho')  #updated to align with product
     and lower(last_touch_feature_desc) not in ('end cards')
     and last_touch_feature_detail_desc not in ('Homepage Carousel - Keep Watching','Content Page - Episodes')
-group by 1,2,3,4)
+group by 1,2,3,4),
+
+stage4_discovery_streams as (
+
+select distinct
+v69_registration_id_nbr,
+video_playback_ind,
+day_dt,
+reporting_series_nm,
+last_touch_feature_desc,
+v88_player_session_nbr
+from `ent_vw.ctd_path_summary_day` a join stage1_originals b on a.reporting_series_nm = b.title
+where day_dt between '2022-01-01' and current_date()
+    and reporting_series_nm != ''
+and v69_registration_id_nbr is not null
+and v69_registration_id_nbr != ''
+    and last_touch_feature_desc is not null
+    and report_suite_id_nm not in (
+      'cbsicbsau',
+      'cbsicbsca',
+      'cbsicbstve'
+    )
+    AND report_suite_id_nm = 'cnetcbscomsite'  #updated to align with product
+    and v1_brand_nm not like 'pplusintl_%'
+    and v1_brand_nm like 'pplus_%'
+    AND LOWER(reporting_content_type_cd) IN ('movie', 'fep','live')  #updated to align with product
+    AND LOWER(subscription_state_desc) IN ("trial", "sub", "discount offer")  #updated to align with product
+    AND LOWER(subscription_package_desc) IN ('cf', 'lc', 'lcp', 'cf-sho', 'lc-sho', 'lcp-sho')  #updated to align with product
+    and last_touch_feature_desc is not null
+    and stream_duration_sec_qty > 0
+order by 1,2,3,4,5
+),
+
+stage5_first_stream as (
+select a.*,
+b.first_stream_day
+from stage4_discovery_streams a
+join (select distinct
+v69_registration_id_nbr,
+v88_player_session_nbr,
+reporting_series_nm,
+first_stream_day
+from `i-dss-ent-data.temp_mk.user_first_content_stream` a join stage1_originals b on a.reporting_series_nm = b.title
+where reporting_series_nm is not null
+and reporting_series_nm != ''
+and v69_registration_id_nbr is not null
+and v69_registration_id_nbr != ''
+and total_minutes > 0) b
+on a.v69_registration_id_nbr=b.v69_registration_id_nbr
+and a.reporting_series_nm=b.reporting_series_nm
+and a.v88_player_session_nbr=b.v88_player_session_nbr
+and a.day_dt=b.first_stream_day
+),
+
+stage6_discovery_streams_final as (
+select   
+day_dt, 
+reporting_series_nm,
+       case when lower(last_touch_feature_desc) like '%homepage%carousel%' then 'Homepage Carousel'
+       when lower(last_touch_feature_desc) like 'homepage' then 'Homepage Carousel'
+       when lower(last_touch_feature_desc) like '%content page%' then 'Content Page'
+       when lower(last_touch_feature_desc) like '%cbs%' then 'CBS Brand Hub'
+       when lower(last_touch_feature_desc) like '%nickelodeon%' then 'Nickelodeon Brand Hub'
+       when lower(last_touch_feature_desc) like '%mtv%' then 'MTV Brand Hub'
+       when lower(last_touch_feature_desc) like '%smithsonian%' then 'Smithsonian Brand Hub'
+       when lower(last_touch_feature_desc) like '%comedy central%' then 'CC Brand Hub'
+       when lower(last_touch_feature_desc) like '%bet%' then 'BET Brand Hub'
+       when lower(last_touch_feature_desc) like '%showtime%' then 'Showtime Hub'
+       when lower(last_touch_feature_desc) like '%thematic%' then 'Thematic Hub'
+       else last_touch_feature_desc end last_touch,
+"Includes All Features" flag,
+"Discovery Streams" stream_type,
+sum(video_playback_ind) streams,
+from stage5_first_stream
+group by 1,2,3
+
+
+)
 
 select * from stage2_originals_streaming
 union all 
-select * from stage3_overall)
+select * from stage3_overall
+union all 
+select * from stage6_discovery_streams_final)
